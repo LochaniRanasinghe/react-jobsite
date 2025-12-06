9444ccadaf9b18f68ca3ebc573527a1dc6e06717
LochaniRanasinghe <lochaniransinghe2000@gmail.com>
2025-12-06 10:29:36 +0530

    Add ViewAllJobs component and update App to include it; enhance JobListing with show more/less functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero.jsx";
 import HomeCards from "./components/HomeCards.jsx";
 import Card from "./components/Card.jsx";
 import JobListings from "./components/JobListings.jsx";
+import ViewAllJobs from "./components/ViewAllJobs.jsx";
 
 const App = () => {
   return (
@@ -11,16 +12,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-      
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />    
     </>
   );
 };
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import {useState, useEffect} from "react";
 
 const JobListing = ({ job }) => {
+
+  const[showFullDescription, setShowFullDescription] = useState(false);
+  let description = job.description;
+
+  if(!showFullDescription){
+    description = description.substring(0, 90) + "... ";
+  }
+
   return (
     <div>
       <div key={job.id} className="bg-white rounded-xl shadow-md relative">
@@ -10,7 +19,11 @@ const JobListing = ({ job }) => {
             <div className="text-gray-600 my-2">{job.type}</div>
             <h3 className="text-xl font-bold">{job.title}</h3>
           </div>
-          <div className="mb-5">{job.description}</div>
+          <div className="mb-5">{description}</div>
+
+          <button onClick={() => setShowFullDescription(prevState => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-700">
+            {showFullDescription ? "Show Less" : "Show More"}
+          </button>
 
           <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
           <div className="border border-gray-100 mb-5"></div>
