91a9b5e46ddd331b627d74abfff81491f20b3e1b
LochaniRanasinghe <lochaniransinghe2000@gmail.com>
2025-12-06 09:42:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import HomeCards from "./components/HomeCards.jsx";
+import Card from "./components/Card.jsx";
+import JobListings from "./components/JobListings.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default App;
